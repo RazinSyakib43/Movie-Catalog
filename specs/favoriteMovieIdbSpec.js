26c3a8480e866efce9a885023d62a3b1a1b56d79
Muhammad Razin Syakib <muhammadrazin14@gmail.com>
2022-11-15 02:59:55 +0700

    Testing

---
+++ specs/favoriteMovieIdbSpec.js
@@ -0,0 +1,56 @@
+import { itActsAsFavoriteMovieModel } from './contract/favoriteMovieContract';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+
+let favoriteMovies = [];
+
+const FavoriteMovieArray = {
+  getMovie(id) {
+    if (!id) {
+      return;
+    }
+
+    // eslint-disable-next-line eqeqeq
+    return favoriteMovies.find((movie) => movie.id == id);
+  },
+
+  getAllMovies() {
+    return favoriteMovies;
+  },
+
+  putMovie(movie) {
+    if (!movie.hasOwnProperty('id')) {
+      return;
+    }
+
+    // pastikan id ini belum ada dalam daftar favoriteMovies
+    if (this.getMovie(movie.id)) {
+      return;
+    }
+
+    favoriteMovies.push(movie);
+  },
+
+  deleteMovie(id) {
+    // cara boros menghapus film dengan meng-copy film yang ada
+    // kecuali film dengan id == id
+    // eslint-disable-next-line eqeqeq
+    favoriteMovies = favoriteMovies.filter((movie) => movie.id != id);
+  },
+};
+
+describe('Favorite Movie Idb Contract Test Implementation', () => {
+  afterEach(async () => {
+    (await FavoriteMovieIdb.getAllMovies()).forEach(async (movie) => {
+      await FavoriteMovieIdb.deleteMovie(movie.id);
+    });
+  });
+
+  itActsAsFavoriteMovieModel(FavoriteMovieIdb);
+});
+
+describe('Favorite Movie Array Contract Test Implementation', () => {
+  // eslint-disable-next-line no-return-assign
+  afterEach(() => favoriteMovies = []);
+
+  itActsAsFavoriteMovieModel(FavoriteMovieArray);
+});
---
+++ specs/helpers/testFactories.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import LikeButtonInitiator from '../../src/scripts/utils/like-button-initiator';
+import FavoriteMovieIdb from '../../src/scripts/data/favorite-movie-idb';
+
+const createLikeButtonPresenterWithMovie = async (movie) => {
+  await LikeButtonInitiator.init({
+    likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    movie,
+    favoriteMovies: FavoriteMovieIdb,
+  });
+};
+
+export { createLikeButtonPresenterWithMovie };
